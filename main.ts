613d2d3d093e3dec3d39937e6f39d7de2f4f180a
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2024-01-05 13:36:06 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+function LogData () {
+    basic.showString(data_string)
+    readings = data_string.split(",")
+    if (readings.length == 5) {
+        got5 = true
+        temperature = parseFloat(readings[0])
+        pressure = parseFloat(readings[1])
+        humidity = parseFloat(readings[2])
+        wind_speed = parseFloat(readings[3])
+        wind_dir = readings[4]
+    } else {
+        if (readings.length == 1) {
+            got1 = true
+            rain = parseFloat(readings[0])
+        }
+    }
+    if (got5 == true && got1 == true) {
+        datalogger.log(
+        datalogger.createCV("Temp", temperature),
+        datalogger.createCV("Pressure", pressure),
+        datalogger.createCV("Humidity", humidity),
+        datalogger.createCV("WindSpeed", wind_speed),
+        datalogger.createCV("WindDir", wind_dir),
+        datalogger.createCV("Rain", rain)
+        )
+        got1 = false
+        got5 = false
+    }
+}
+radio.onReceivedString(function (receivedString) {
+    data_string = receivedString
+    LogData()
+})
+let rain = 0
+let wind_dir = ""
+let wind_speed = 0
+let humidity = 0
+let pressure = 0
+let temperature = 0
+let readings: string[] = []
+let data_string = ""
+let got1 = false
+let got5 = false
+radio.setGroup(1)
+radio.setFrequencyBand(0)
+got5 = false
+got1 = false
+let debug = false
+loops.everyInterval(60000, function () {
+    if (debug == true) {
+        data_string = "72,1030,45,10,NW"
+        LogData()
+        data_string = "1"
+        LogData()
+    }
+})
