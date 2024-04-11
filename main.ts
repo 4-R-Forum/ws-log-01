a272614457e14b4d579721ae6b0fce91474677dc
4-R-Forum <8607712+4-R-Forum@users.noreply.github.com>
2024-04-11 17:29:59 -0400

    Update Day, Period Num for BenchTest

--- main.ts
+++ main.ts
@@ -1,42 +1,42 @@
+// WS-Log
+// 
+// Download to RED
 function LogData () {
     basic.showString(data_string)
     readings = data_string.split(",")
     if (readings.length == 5) {
-        got5 = true
         temperature = parseFloat(readings[0])
         humidity = parseFloat(readings[1])
         pressure = parseFloat(readings[2])
         wind_speed = parseFloat(readings[3])
         wind_dir = readings[4]
-    } else {
-        if (readings.length == 1) {
-            got1 = true
-            rain = parseFloat(readings[0])
-        }
-    }
-    if (got5 == true && got1 == true) {
         datalogger.log(
         datalogger.createCV("Temp", temperature),
         datalogger.createCV("Pressure", pressure),
         datalogger.createCV("Humidity", humidity),
         datalogger.createCV("WindSpeed", wind_speed),
         datalogger.createCV("WindDir", wind_dir),
-        datalogger.createCV("Rain", rain)
+        datalogger.createCV("Day", daynum),
+        datalogger.createCV("Period", periodnum)
         )
-        got1 = false
-        got5 = false
+    } else {
+        if (readings.length == 1) {
+            rain = parseFloat(readings[0])
+            datalogger.log(
+            datalogger.createCV("Rain", convertToText(rain)),
+            datalogger.createCV("Day", convertToText(daynum))
+            )
+        }
+        periodnum = 0
+        daynum += 1
     }
     basic.clearScreen()
 }
 radio.onReceivedString(function (receivedString) {
     data_string = receivedString
     LogData()
+    periodnum += 1
 })
-/**
- * WS-Log
- * 
- * Download to RED
- */
 let rain = 0
 let wind_dir = ""
 let wind_speed = 0
@@ -45,18 +45,23 @@ let humidity = 0
 let temperature = 0
 let readings: string[] = []
 let data_string = ""
-let got1 = false
-let got5 = false
+let periodnum = 0
+let daynum = 0
+let debug = false
 radio.setGroup(1)
 radio.setFrequencyBand(0)
-got5 = false
-got1 = false
-let debug = false
-loops.everyInterval(60000, function () {
+daynum = 0
+periodnum = 0
+loops.everyInterval(30000, function () {
     if (debug == true) {
         data_string = "72,1030,45,10,NW"
         LogData()
-        data_string = "1"
-        LogData()
+        periodnum += 1
+        if (periodnum > 1) {
+            data_string = convertToText(randint(0, 10) * 0.11)
+            LogData()
+            periodnum = 0
+            daynum += 1
+        }
     }
 })
